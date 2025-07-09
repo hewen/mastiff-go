aef9731beb8eb2e70dc4d90da7f9c136a287fe8a
hewen <wen.bin.he@foxmail.com>
2025-07-09 12:51:47 +0800

    test: WrapHandler add error test case

--- handler/wrapper_test.go
+++ handler/wrapper_test.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"bytes"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -28,6 +29,24 @@ func TestWrapHandler(_ *testing.T) {
 	handlerLog(ctx)
 
 	ctx, _ = gin.CreateTestContext(httptest.NewRecorder())
+	ctx.Request, _ = http.NewRequest("GET", "/test", bytes.NewReader([]byte(`{"test":1}`)))
+	ctx.Request.Header.Add("Content-Type", "application/json")
+
+	wrapHandler(ctx)
+	handlerLog(ctx)
+}
+
+func TestWrapHandlerError(_ *testing.T) {
+	type Test struct {
+		Test int `json:"test"`
+	}
+
+	wrapHandler := WrapHandler(func(_ *gin.Context, req Test) (resp Test, err error) {
+		return req, fmt.Errorf("wapp error")
+	})
+	handlerLog := logging.GinLoggingHandler()
+
+	ctx, _ := gin.CreateTestContext(httptest.NewRecorder())
 	ctx.Request, _ = http.NewRequest("GET", "/test", bytes.NewReader([]byte(`{"test":1}`)))
 	ctx.Request.Header.Add("Content-Type", "application/json")
 
