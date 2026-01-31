80d9a8ee29329041163dbd81357da69d1c093ca5
hewen <wen.bin.he@foxmail.com>
2026-01-31 16:32:11 +0800

    feat: Update redis store configuration and tests

--- store/redis.go
+++ store/redis.go
@@ -2,18 +2,34 @@
 package store
 
 import (
+	"crypto/tls"
+
 	"github.com/go-redis/redis/v7"
 	"github.com/hewen/mastiff-go/config/storeconf"
 )
 
 // InitRedis initializes a Redis connection.
 func InitRedis(conf storeconf.RedisConfig) (*redis.Client, error) {
-	redisConn := redis.NewClient(&redis.Options{
-		Addr:      conf.Addr,
-		Password:  conf.Password,
-		DB:        conf.DB,
-		TLSConfig: conf.TLSConfig,
-	})
+	opt := &redis.Options{
+		Addr:     conf.Addr,
+		Password: conf.Password,
+		DB:       conf.DB,
+	}
+
+	if conf.TLSConfig != nil && conf.TLSConfig.Enabled {
+		var minVersion uint16 = tls.VersionTLS12
+		if conf.TLSConfig.VersionTLS != 0 {
+			minVersion = conf.TLSConfig.VersionTLS
+		}
+
+		opt.TLSConfig = &tls.Config{
+			MinVersion:         minVersion,
+			ServerName:         conf.TLSConfig.ServerName,
+			InsecureSkipVerify: conf.TLSConfig.InsecureSkipVerify, // #nosec
+		}
+	}
+
+	redisConn := redis.NewClient(opt)
 
 	if conf.RegisterHookDriver {
 		hook := &RedisHook{}
--- store/redis_test.go
+++ store/redis_test.go
@@ -1,11 +1,20 @@
 package store
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/tls"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/pem"
+	"math/big"
 	"testing"
+	"time"
 
 	"github.com/alicebob/miniredis/v2"
 	"github.com/hewen/mastiff-go/config/storeconf"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestInitRedis(t *testing.T) {
@@ -16,4 +25,68 @@ func TestInitRedis(t *testing.T) {
 		Addr: s.Addr(),
 	})
 	assert.Nil(t, err)
+
+}
+
+func TestInitTLSRedis(t *testing.T) {
+	certPEM, keyPEM := generateTestCert(t)
+	cert, _ := tls.X509KeyPair(certPEM, keyPEM)
+
+	s, err := miniredis.RunTLS(&tls.Config{
+		MinVersion:   tls.VersionTLS12,
+		Certificates: []tls.Certificate{cert},
+	})
+	assert.Nil(t, err)
+	_, err = InitRedis(storeconf.RedisConfig{
+		Addr: s.Addr(),
+		TLSConfig: &storeconf.TLSConfig{
+			Enabled:            true,
+			VersionTLS:         tls.VersionTLS12,
+			InsecureSkipVerify: true,
+		},
+	})
+	assert.Nil(t, err)
+}
+
+func generateTestCert(t *testing.T) (certPEM, keyPEM []byte) {
+	t.Helper()
+
+	priv, err := rsa.GenerateKey(rand.Reader, 2048)
+	require.NoError(t, err)
+
+	template := x509.Certificate{
+		SerialNumber: big.NewInt(1),
+		Subject: pkix.Name{
+			Organization: []string{"Test Org"},
+		},
+		NotBefore: time.Now().Add(-time.Hour),
+		NotAfter:  time.Now().Add(time.Hour * 24),
+
+		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
+		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
+		BasicConstraintsValid: true,
+
+		DNSNames: []string{"localhost"},
+	}
+
+	derBytes, err := x509.CreateCertificate(
+		rand.Reader,
+		&template,
+		&template,
+		&priv.PublicKey,
+		priv,
+	)
+	require.NoError(t, err)
+
+	certPEM = pem.EncodeToMemory(&pem.Block{
+		Type:  "CERTIFICATE",
+		Bytes: derBytes,
+	})
+
+	keyPEM = pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(priv),
+	})
+
+	return
 }
